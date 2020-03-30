b3f0cb3ee1c8e9599576479200d5cf7b6b26d556
Muhammad Salman <salmanmanekia@gmail.com>
2020-03-30 15:52:54 +0300

    Run the bluewalker app with non root user

--- azure-iot-edge/modules/BlueWalker/bluewalker.sh
+++ azure-iot-edge/modules/BlueWalker/bluewalker.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 SOCKETPATH="/tmp/ruuvi.sock"
 
-hciconfig hci0 down
-bluewalker -device hci0 -ruuvi -active -json -duration -1 -unix $SOCKETPATH
+sudo hciconfig hci0 down
+sudo bluewalker -device hci0 -ruuvi -active -json -duration -1 -unix $SOCKETPATH
